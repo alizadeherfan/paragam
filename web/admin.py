e6330ec2d66ac3b11a5cc22bc4faf454c5c43e0a
erfan alizadeh <alizadeh.otari@gmail.com>
2017-01-26 03:48:07 +0330

    incomme

--- web/admin.py
+++ web/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Market
+from .models import Market, Income
 # Register your models here.
 admin.site.register(Market)
+admin.site.register(Income)
